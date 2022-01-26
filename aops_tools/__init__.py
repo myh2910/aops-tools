d7f45cb0549410241764d51e60ca85626cc0f32e
myh2910 <minleeyohan@gmail.com>
2022-01-25 20:25:53 -0500

    Initial commit

---
+++ aops_tools/__init__.py
@@ -0,0 +1,111 @@
+from .extract import extract_topic_info
+from bs4 import BeautifulSoup
+import os
+
+def show_topic_info(
+	topic_code,
+	verbose=False,
+	silent=False,
+	write_files=False,
+	outdir="community",
+	thanker_to_find=None
+):
+	topic_dict = extract_topic_info(topic_code)
+	topic_url = topic_dict["url"]
+	topic_subject = topic_dict["subject"]
+	topic_tags = topic_dict["tags"]
+	topic_source = topic_dict["source"]
+	topic_posts = topic_dict["posts"]
+
+	topic_info = f"Topic url: {topic_url}\nSubject: {topic_subject}"
+	if topic_tags:
+		topic_info += f"\nTags: {', '.join(topic_tags)}"
+	if topic_source:
+		topic_info += f"\nSource: {topic_source}"
+	if topic_posts:
+		topic_info += f"\nPost count: {len(topic_posts)}"
+
+	if verbose or not silent:
+		print(topic_info)
+
+	if write_files:
+		topic_dir = os.path.join(outdir, topic_code)
+		if not os.path.exists(topic_dir):
+			os.makedirs(topic_dir)
+		with open(os.path.join(topic_dir, "info.txt"), "w", encoding="utf8") as topic_info_file:
+			topic_info_file.write(topic_info)
+
+	if thanker_to_find:
+		if verbose or not silent:
+			print("-" * 100)
+		print(f"Posts liked by {thanker_to_find}:")
+		thanker_found = False
+
+	for post_dict in topic_posts:
+		post_number = post_dict["number"]
+		post_url = post_dict["url"]
+		post_username = post_dict["username"]
+		post_user_profile = post_dict["user-profile"]
+		post_date = post_dict["date"]
+		post_edit_info = post_dict["edit-info"]
+		post_thank_count = post_dict["thank-count"]
+		post_thankers = post_dict["thankers"]
+		post_html = post_dict["html"]
+
+		post_info = f"""Post number: {post_number}
+Post url: {post_url}
+Username: {post_username}
+User profile: {post_user_profile}
+Date: {post_date}"""
+		if post_edit_info:
+			post_info += f"\nEdit info: {post_edit_info}"
+		post_info += f"\nThank count: {post_thank_count}"
+		if post_thankers:
+			post_info += f"\nThankers: {', '.join(post_thankers)}"
+
+		if verbose:
+			print("-" * 100)
+			print(post_info)
+
+		if write_files:
+			post_dir = os.path.join(topic_dir, post_number)
+			if not os.path.exists(post_dir):
+				os.makedirs(post_dir)
+
+			with open(os.path.join(post_dir, "info.txt"), "w", encoding="utf8") as post_info_file:
+				post_info_file.write(post_info)
+
+			with open(os.path.join(post_dir, "content.html"), "w", encoding="utf8") as post_html_file:
+				soup = BeautifulSoup(post_html, "lxml")
+				for html_img in soup.find_all("img"):
+					if html_img["src"].startswith("//"):
+						html_img["src"] = "https:" + html_img["src"]
+				html_head = soup.new_tag("head")
+				html_style = soup.new_tag("style")
+				html_style.string = """
+body {
+	line-height: 19px;
+	font-family: Roboto, sans-serif;
+	font-size: 15px;
+	color: #333;
+}
+
+img.asy-image, img.latexcenter {
+	display: block;
+	margin: auto;
+	padding: 1em 0;
+	box-sizing: content-box;
+	-moz-transform: rotate(0);
+	max-width: 100%;
+	height: auto;
+}"""
+				html_head.append(html_style)
+				soup.find("body").insert_before(html_head)
+				post_html_file.write(str(soup))
+
+		if thanker_to_find and thanker_to_find in post_thankers:
+			thanker_found = True
+			print(f"Post #{post_number} ({post_url})")
+
+	if thanker_to_find and not thanker_found:
+		print("Not found")
---
+++ aops_tools/extract.py
@@ -0,0 +1,116 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium_stealth import stealth
+
+def get_hidden_thankers(i, all_hidden=False):
+	if all_hidden:
+		if i == 1:
+			return "1 user"
+		return f"{i} users"
+	if i == 1:
+		return "and 1 other user"
+	return f"and {i} other users"
+
+def extract_topic_info(topic_code):
+	options = webdriver.ChromeOptions()
+	options.add_argument("start-maximized")
+	options.add_argument("--headless")
+	options.add_experimental_option("excludeSwitches", ["enable-automation"])
+	options.add_experimental_option("useAutomationExtension", False)
+	driver = webdriver.Chrome(options=options)
+
+	stealth(
+		driver,
+		languages=["en-US", "en"],
+		vendor="Google Inc.",
+		platform="Win32",
+		webgl_vendor="Intel Inc.",
+		renderer="Intel Iris OpenGL Engine",
+		fix_hairline=True
+	)
+
+	aops_url = "https://artofproblemsolving.com/community"
+	topic_url = aops_url + "/" + topic_code
+	driver.get(topic_url)
+
+	aops_scroll_inner = WebDriverWait(driver, 10).until(EC.visibility_of_element_located(
+		(By.CLASS_NAME, "cmty-postbox-inner-box")
+	)).find_element(By.XPATH, "..")
+	prev_height = 0
+
+	while True:
+		time.sleep(1)
+		driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", aops_scroll_inner)
+		curr_height = driver.execute_script("return arguments[0].scrollHeight", aops_scroll_inner)
+		if curr_height == prev_height:
+			break
+		prev_height = curr_height
+
+	cmty_topic_subject = driver.find_element(By.CLASS_NAME, "cmty-topic-subject").text
+	cmty_item_tag = [tag.text for tag in driver.find_elements(
+		By.XPATH, '//div[@class="cmty-tags-itembox-wrapper"]//a//div'
+	)]
+	cmty_topic_source = driver.find_element(By.CLASS_NAME, "cmty-topic-source-display").text
+	if cmty_topic_source:
+		cmty_topic_source = cmty_topic_source[8:]
+
+	aops_dict = {
+		"url": topic_url,
+		"subject": cmty_topic_subject,
+		"tags": cmty_item_tag,
+		"source": cmty_topic_source,
+		"posts": []
+	}
+
+	for cmty_post in driver.find_elements(By.CLASS_NAME, "cmty-post"):
+		# Get post number and url
+		cmty_post_number = cmty_post.find_element(By.CLASS_NAME, "cmty-post-number")
+		cmty_post_number.click()
+		cmty_post_direct_modal = driver.find_element(By.CLASS_NAME, "cmty-post-direct-modal")
+		post_url = cmty_post_direct_modal.find_element(By.TAG_NAME, "input").get_attribute("value")
+		driver.find_element(By.CLASS_NAME, "aops-close-x").click()
+
+		# Get post html, username, user profile, date and edit info
+		post_html = cmty_post.find_element(By.CLASS_NAME, "cmty-post-html").get_attribute("innerHTML")
+		cmty_post_username = cmty_post.find_element(By.CLASS_NAME, "cmty-post-username")
+		post_user_profile = cmty_post_username.find_element(By.TAG_NAME, "a").get_attribute("href")
+		post_date = cmty_post.find_element(By.CLASS_NAME, "cmty-post-date").text
+		post_edit_info = cmty_post.find_element(By.CLASS_NAME, "cmty-post-edit-info").text
+
+		# Get post thank count and thankers
+		cmty_post_thank_count = cmty_post.find_element(By.CLASS_NAME, "cmty-post-thank-count")
+		post_thank_count = cmty_post_thank_count.text
+		if post_thank_count:
+			post_thank_count = int(post_thank_count.split()[1])
+			cmty_post_thank_count.click()
+			cmty_thankers = cmty_post.find_element(By.CLASS_NAME, "cmty-thankers").text.split(", ")
+			visible_thank_count = len(cmty_thankers)
+			hidden_thankers = cmty_thankers[-1]
+			if visible_thank_count == 1:
+				if hidden_thankers == get_hidden_thankers(post_thank_count, True):
+					cmty_thankers = []
+				elif post_thank_count > 1:
+					cmty_thankers = [hidden_thankers[:-len(get_hidden_thankers(post_thank_count - 1)) - 1]]
+			elif hidden_thankers == get_hidden_thankers(post_thank_count - visible_thank_count + 1):
+				del cmty_thankers[-1]
+		else:
+			post_thank_count = 0
+			cmty_thankers = []
+
+		aops_dict["posts"].append({
+			"number": cmty_post_number.text[1:],
+			"url": post_url,
+			"html": post_html,
+			"username": cmty_post_username.text,
+			"user-profile": post_user_profile,
+			"date": post_date,
+			"edit-info": post_edit_info,
+			"thank-count": post_thank_count,
+			"thankers": cmty_thankers
+		})
+
+	driver.quit()
+	return aops_dict
