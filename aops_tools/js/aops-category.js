93a6e22046b931600064a2d117e7e2dcb2cd2ea1
Yohan Min <minleeyohan@gmail.com>
2023-05-16 23:19:51 +0900

    ESLint

--- aops_tools/js/aops-category.js
+++ aops_tools/js/aops-category.js
@@ -1,76 +1,73 @@
 // Get category data from AoPS
 const focusCategory = await scrollDown();
-const categoryUrl = 'https://artofproblemsolving.com/community/c';
-let categoryData = {category_url: `${categoryUrl}${focusCategory.category_id}`};
+const categoryUrl = "https://artofproblemsolving.com/community/c";
+let categoryData = {
+  category_url: `${categoryUrl}${focusCategory.category_id}`,
+};
 
 const categoryKeys = [
-	'category_id',
-	'category_name',
-	'category_type',
-	'short_description'
+  "category_id",
+  "category_name",
+  "category_type",
+  "short_description",
 ];
-categoryKeys.forEach(key => categoryData[key] = focusCategory[key]);
+categoryKeys.forEach((key) => (categoryData[key] = focusCategory[key]));
 
 switch (focusCategory.category_type) {
-	case 'folder':
-		const folderKeys = [
-			'item_id',
-			'item_subtitle',
-			'item_text',
-			'item_type',
-		];
-		categoryData.items = focusCategory.items.map(data => {
-			let itemData = {item_url: `${categoryUrl}${data.item_id}`};
-			folderKeys.forEach(key => itemData[key] = data[key]);
-			return itemData;
-		});
-		break;
-	case 'view_posts':
-		const viewPostsKeys = [
-			'category_id',
-			'category_name',
-			'post_canonical',
-			'post_format',
-			'post_id',
-			'post_number',
-			'post_rendered',
-			'post_type',
-			'poster_avatar',
-			'poster_id',
-			'topic_id',
-			'username'
-		];
-		categoryData.items = focusCategory.items.map(data => {
-			let postData = data.post_data;
-			let itemData = {
-				item_text: data.item_text,
-				item_type: data.item_type,
-				post_data: {}
-			};
-			viewPostsKeys.forEach(key => itemData.post_data[key] = postData[key]);
-			if (postData.post_type === 'forum') {
-				itemData.post_data.post_url = `${categoryUrl}${postData.category_id}h${postData.topic_id}p${postData.post_id}`;
-			}
-			return itemData;
-		});
-		break;
+  case "folder":
+    const folderKeys = ["item_id", "item_subtitle", "item_text", "item_type"];
+    categoryData.items = focusCategory.items.map((data) => {
+      let itemData = { item_url: `${categoryUrl}${data.item_id}` };
+      folderKeys.forEach((key) => (itemData[key] = data[key]));
+      return itemData;
+    });
+    break;
+  case "view_posts":
+    const viewPostsKeys = [
+      "category_id",
+      "category_name",
+      "post_canonical",
+      "post_format",
+      "post_id",
+      "post_number",
+      "post_rendered",
+      "post_type",
+      "poster_avatar",
+      "poster_id",
+      "topic_id",
+      "username",
+    ];
+    categoryData.items = focusCategory.items.map((data) => {
+      let postData = data.post_data;
+      let itemData = {
+        item_text: data.item_text,
+        item_type: data.item_type,
+        post_data: {},
+      };
+      viewPostsKeys.forEach((key) => (itemData.post_data[key] = postData[key]));
+      if (postData.post_type === "forum") {
+        itemData.post_data.post_url = `${categoryUrl}${postData.category_id}h${postData.topic_id}p${postData.post_id}`;
+      }
+      return itemData;
+    });
+    break;
 }
 return categoryData;
 
 // Functions
 async function scrollDown() {
-	while (!getFocusCategory()) {
-		await new Promise(resolve => setTimeout(resolve, 10));
-	}
-	if (!getFocusCategory().attributes.is_forum) {
-		while (!getFocusCategory().attributes.no_more_items) {
-			window.scrollTo(0, document.body.scrollHeight);
-			await new Promise(resolve => requestAnimationFrame(resolve));
-		}
-	}
-	return getFocusCategory().attributes;
+  while (!getFocusCategory()) {
+    await new Promise((resolve) => setTimeout(resolve, 10));
+  }
+  if (!getFocusCategory().attributes.is_forum) {
+    while (!getFocusCategory().attributes.no_more_items) {
+      window.scrollTo(0, document.body.scrollHeight);
+      await new Promise((resolve) => requestAnimationFrame(resolve));
+    }
+  }
+  return getFocusCategory().attributes;
 }
 
 function getFocusCategory() {
-	return AoPS.Community.MasterModel.attributes.focus_category;
+  return AoPS.Community.MasterModel.attributes.focus_category;
 }
--- aops_tools/js/aops-topic.js
+++ aops_tools/js/aops-topic.js
@@ -3,67 +3,67 @@ const focusTopic = await scrollDown();
 const topicUrl = `https://artofproblemsolving.com/community/c${focusTopic.category_id}h${focusTopic.topic_id}`;
 
 const postKeys = [
-	'avatar',
-	'last_edit_reason',
-	'last_edit_time',
-	'last_editor_username',
-	'num_edits',
-	'num_posts',
-	'post_canonical',
-	'post_format',
-	'post_id',
-	'post_number',
-	'post_rendered',
-	'post_time',
-	'poster_id',
-	'thankers',
-	'thanks_received',
-	'username'
+  "avatar",
+  "last_edit_reason",
+  "last_edit_time",
+  "last_editor_username",
+  "num_edits",
+  "num_posts",
+  "post_canonical",
+  "post_format",
+  "post_id",
+  "post_number",
+  "post_rendered",
+  "post_time",
+  "poster_id",
+  "thankers",
+  "thanks_received",
+  "username",
 ];
 let topicData = {
-	posts_data: focusTopic.posts.models.map(model => {
-		let postData = {post_url: `${topicUrl}p${model.attributes.post_id}`};
-		postKeys.forEach(key => postData[key] = model.attributes[key]);
-		return postData;
-	}),
-	topic_url: topicUrl
+  posts_data: focusTopic.posts.models.map((model) => {
+    let postData = { post_url: `${topicUrl}p${model.attributes.post_id}` };
+    postKeys.forEach((key) => (postData[key] = model.attributes[key]));
+    return postData;
+  }),
+  topic_url: topicUrl,
 };
 
 const topicKeys = [
-	'announce_type',
-	'category_id',
-	'category_name',
-	'locked',
-	'num_deleted',
-	'num_posts',
-	'num_views',
-	'source',
-	'tags',
-	'topic_id',
-	'topic_title'
+  "announce_type",
+  "category_id",
+  "category_name",
+  "locked",
+  "num_deleted",
+  "num_posts",
+  "num_views",
+  "source",
+  "tags",
+  "topic_id",
+  "topic_title",
 ];
-topicKeys.forEach(key => topicData[key] = focusTopic[key]);
+topicKeys.forEach((key) => (topicData[key] = focusTopic[key]));
 return topicData;
 
 // Functions
 async function scrollDown() {
-	while (!getFocusTopic()) {
-		await new Promise(resolve => setTimeout(resolve, 10));
-	}
-	if (!getFocusTopic().attributes.all_posts_fetched) {
-		const selector = '.cmty-postbox-inner-box';
-		while (document.querySelector(selector) === null) {
-			await new Promise(resolve => setTimeout(resolve, 10));
-		}
-		const scrollBar = document.querySelector(selector).parentElement;
-		do {
-			scrollBar.scrollTo(0, scrollBar.scrollHeight);
-			await new Promise(resolve => requestAnimationFrame(resolve));
-		} while (!getFocusTopic().attributes.all_posts_fetched)
-	}
-	return getFocusTopic().attributes;
+  while (!getFocusTopic()) {
+    await new Promise((resolve) => setTimeout(resolve, 10));
+  }
+  if (!getFocusTopic().attributes.all_posts_fetched) {
+    const selector = ".cmty-postbox-inner-box";
+    while (document.querySelector(selector) === null) {
+      await new Promise((resolve) => setTimeout(resolve, 10));
+    }
+    const scrollBar = document.querySelector(selector).parentElement;
+    do {
+      scrollBar.scrollTo(0, scrollBar.scrollHeight);
+      await new Promise((resolve) => requestAnimationFrame(resolve));
+    } while (!getFocusTopic().attributes.all_posts_fetched);
+  }
+  return getFocusTopic().attributes;
 }
 
 function getFocusTopic() {
-	return AoPS.Community.MasterModel.attributes.focus_topic;
+  return AoPS.Community.MasterModel.attributes.focus_topic;
 }
